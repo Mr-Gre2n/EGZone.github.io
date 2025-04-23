2118daece39d2466637b79e76fe01dc2a8d6a60b
adham essam <86185985+adhamesaam@users.noreply.github.com>
2025-04-23 08:48:10 +0200

    created a cart counter function

--- JS/navbarScript.js
+++ JS/navbarScript.js
@@ -4,13 +4,14 @@
 const CART_COUNT = document.getElementById("cartCount");
 const BTN_ACCOUNT = document.getElementById("btnAccount");
 
-
+const Search = document.getElementById("searchBar")
+const Results = document.getElementById("results")
+var data = localStorage.getItem("title")
 /***********************
 *      Variables
 ***********************/
-
-/***********************
-*      Methods
+/*********************** 
+*    cart counter
 ***********************/
 
 /***********************
@@ -21,11 +22,40 @@ function checkUser() {
     console.log(LoggedInUser)
     if (LoggedInUser) {
         BTN_ACCOUNT.innerText = LoggedInUser[0].name;
-    }else{
+    }
+    else{
         BTN_ACCOUNT.innerText = "Login";
     }
 }
 checkUser();
+
 /***********************
-*     UI Events
+*  Update cart count
 ***********************/
+
+localStorage.setItem('cartItems', JSON.stringify(cartItems));
+
+function updateCartCountDisplay(cartItems) {
+    
+    function calculateTotalItems(cartItems) {
+        let totalItems = 0;
+    
+        for (let i = 0; i < cartItems.length; i++) {
+            totalItems += cartItems[i].quantity;
+        }
+    
+        return totalItems;
+    }
+
+    
+    const totalItems = calculateTotalItems(cartItems)
+    
+        cartCountElement.textContent = totalItems;
+    
+        if (totalItems > 0) {
+            cartCountElement.style.display = 'flex';
+        } else {
+            cartCountElement.style.display = 'none';
+        }
+} 
+updateCartCountDisplay(cartItems);
