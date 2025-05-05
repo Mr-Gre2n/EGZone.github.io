152a5383287b7e2fa95a2db1e761516d437df7ad
Mr-Gre2n <mr.gre2n@gmail.com>
2025-05-05 23:07:09 +0300

    fix profile bugs

--- JS/profilePageScript.js
+++ JS/profilePageScript.js
@@ -9,46 +9,51 @@ const sidebarName = document.getElementById('sidebar-name');
 const sidebarEmail = document.getElementById('sidebar-email');
 const save_btn = document.getElementById('save');
 
+// Check if user is logged in
+let loggedInUser = JSON.parse(localStorage.getItem('LoggedInUser')); 
+
 function dataFromStorage() {
-    // Check if user is logged in
-    const USER = localStorage.getItem('LoggedInUser'); 
-    if(!USER){
+    if(!loggedInUser){
         show404Page();
         return;
     }
-    
     // Fill the form with the data
-    firstNameInput.value = USER.firstName;
-    lastNameInput.value = USER.LastName;
-    emailInput.value = USER.Email;
-    phoneInput.value = USER.Phone;
+    firstNameInput.value = loggedInUser.firstName;
+    lastNameInput.value = loggedInUser.LastName;
+    emailInput.value = loggedInUser.Email;
+    phoneInput.value = loggedInUser.Phone;
     
     // Update sidebar info
-    sidebarName.textContent = USER.firstName + ' ' + USER.LastName;
-    sidebarEmail.textContent = USER.Email;
+    sidebarName.textContent = loggedInUser.firstName + ' ' + loggedInUser.LastName;
+    sidebarEmail.textContent = loggedInUser.Email;
 }
 
 dataFromStorage();
 
 // Save changes
 
-save_btn.ATTRIBUTE_NODE('click',function() {
+save_btn.addEventListener('click',function() {
     // validation
     if (!firstNameInput.value || !lastNameInput.value || !emailInput.value) {
         alert('Please fill out all required fields');
         return;
     }
-    
-    // Save to localStorage
-    localStorage.setItem('firstName', firstNameInput.value);
-    localStorage.setItem('lastName', lastNameInput.value);
-    localStorage.setItem('email', emailInput.value);
-    localStorage.setItem('phone', phoneInput.value);
-    
+    loggedInUser.firstName = firstNameInput.value;    
+    loggedInUser.LastName = lastNameInput.value;    
+    loggedInUser.Email = emailInput.value;    
+    loggedInUser.Phone = phoneInput.value;    
     // Update sidebar info
     sidebarName.textContent = firstNameInput.value + lastNameInput.value;
     sidebarEmail.textContent = emailInput.value;
-    
+
+    // Save to localStorage    
+    let users = JSON.parse(localStorage.getItem('Users')); 
+    const user = users.find(user => user.ID == loggedInUser.ID);
+    users.splice(users.indexOf(user), 1, loggedInUser);
+
+    localStorage.setItem('LoggedInUser', JSON.stringify(loggedInUser));   
+    localStorage.setItem('Users', JSON.stringify(users));    
+
     alert('Account information saved successfully!');
 });
 
